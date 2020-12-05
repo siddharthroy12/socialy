b932353a4d6579a112a9b3eb0df6cac6915868bc
Siddharth Roy <siddharthroy36912@gmail.com>
2020-12-05 17:49:49 +0530

    Fixed graphql uri in client and updated favicon

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: '/'
+  uri: '/graphql'
 })
 
 const authLink = setContext(() => {
